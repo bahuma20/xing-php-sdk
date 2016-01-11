eacf87744f08db4dd95d54e050e9d14356f94215
Max Bachhuber <bahuma20@bahuma.io>
2016-01-11 04:08:57 -0500

    Applied fixes from StyleCI

--- src/Bahuma/XingSDK/XingSDK.php
+++ src/Bahuma/XingSDK/XingSDK.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Bahuma\XingSDK;
 
 use GuzzleHttp\Client;
@@ -8,26 +9,24 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 
 /**
- * Class XingSDK
- * @package Bahuma\XingSDK
+ * Class XingSDK.
  */
 class XingSDK {
-
     /**
      * @var Client
      */
     private $client;
 
     /**
-     * Creates a new instance of the XingSDK
+     * Creates a new instance of the XingSDK.
+     *
      * @param array $config
      */
-    function __construct(array $config) {
+    public function __construct(array $config) {
         $token = '';
         $token_secret = '';
 
         $stack = HandlerStack::create();
-
 
         $oauth_middleware = new Oauth1($config);
 
@@ -35,24 +34,25 @@ class XingSDK {
 
         $this->client = new Client([
             'base_uri' => 'https://api.xing.com/v1/',
-            'handler' => $stack,
-            'auth' => 'oauth',
-            'headers' => [
-                'Accept' => 'application/json',
+            'handler'  => $stack,
+            'auth'     => 'oauth',
+            'headers'  => [
+                'Accept'       => 'application/json',
                 'Content-Type' => 'application/json',
             ],
         ]);
     }
 
     /**
-     * Converts a urlencoded response from the api to an associative array
+     * Converts a urlencoded response from the api to an associative array.
      *
      * @param StreamInterface $body The body of the response
+     *
      * @return array An associative array with the keys and values from the response
      */
     private static function decodeUrlEncodedResponse(StreamInterface $body) {
         $body = $body->getContents();
-        $decodedBody = array();
+        $decodedBody = [];
         $body = explode('&', $body);
 
         foreach($body as $key => $value) {
@@ -64,9 +64,10 @@ class XingSDK {
     }
 
     /**
-     * Converts a response into an array or stdClass using the json_decode function
+     * Converts a response into an array or stdClass using the json_decode function.
      *
      * @param ResponseInterface $response The response to decode
+     *
      * @return mixed An array or stdClass
      */
     public static function decodeResponse(ResponseInterface $response) {
@@ -78,16 +79,17 @@ class XingSDK {
     }
 
     /**
-     * Obtain a request token which can be used to obtain an request token
+     * Obtain a request token which can be used to obtain an request token.
      *
      * @param string $callback A URL on your website where the users are redirected to after accepting the permissions
+     *
      * @return array An array containing the request_token, the request_token_secret and the authorize_url
      */
     public function getRequestToken($callback_url) {
         $response = $this->client->post('request_token', [
             'query' => [
                 'oauth_callback' => $callback_url,
-            ]
+            ],
         ]);
 
         $tokens = self::decodeUrlEncodedResponse($response->getBody());
@@ -95,16 +97,17 @@ class XingSDK {
         $request_token_secret = $tokens['oauth_token_secret'];
 
         return [
-            'request_token' => $request_token,
+            'request_token'        => $request_token,
             'request_token_secret' => $request_token_secret,
-            'authorize_url' => 'https://api.xing.com/v1/authorize?oauth_token='. $request_token,
+            'authorize_url'        => 'https://api.xing.com/v1/authorize?oauth_token='.$request_token,
         ];
     }
 
     /**
-     * Obtain an access token which can be stored for permanent access
+     * Obtain an access token which can be stored for permanent access.
      *
      * @param string $oauth_verifier The the url parameter "oauth_verifier" which is present at the redirect callback after the user has acceppted the permissions
+     *
      * @return array An array containing the access_token and the access_token_secret. These should be stored if you want permanent access.
      */
     public function getAccessToken($oauth_verifier) {
@@ -120,7 +123,7 @@ class XingSDK {
         $access_token_secret = $tokens['oauth_token_secret'];
 
         return [
-            'access_token' => $access_token,
+            'access_token'        => $access_token,
             'access_token_secret' => $access_token_secret,
         ];
     }
